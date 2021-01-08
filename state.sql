62706b96f1fa19f31c9e8a5d9115e043e1e4b0c1
codepreacher <75647192+code-preacher@users.noreply.github.com>
2021-01-08 00:40:46 -0800

    Add files via upload

---
+++ state.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 08, 2021 at 09:33 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dopatech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `state`
+--
+
+CREATE TABLE `state` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `state`
+--
+
+INSERT INTO `state` (`id`, `name`) VALUES
+(1, 'Abia State'),
+(2, 'Adamawa State'),
+(3, 'Akwa Ibom State'),
+(4, 'Anambra State'),
+(5, 'Bauchi State'),
+(6, 'Bayelsa State'),
+(7, 'Benue State'),
+(8, 'Borno State'),
+(9, 'Cross River State'),
+(10, 'Delta State'),
+(11, 'Ebonyi State'),
+(12, 'Edo State'),
+(13, 'Ekiti State'),
+(14, 'Enugu State'),
+(15, 'FCT'),
+(16, 'Gombe State'),
+(17, 'Imo State'),
+(18, 'Jigawa State'),
+(19, 'Kaduna State'),
+(20, 'Kano State'),
+(21, 'Katsina State'),
+(22, 'Kebbi State'),
+(23, 'Kogi State'),
+(24, 'Kwara State'),
+(25, 'Lagos State'),
+(26, 'Nasarawa State'),
+(27, 'Niger State'),
+(28, 'Ogun State'),
+(29, 'Ondo State'),
+(30, 'Osun State'),
+(31, 'Oyo State'),
+(32, 'Plateau State'),
+(33, 'Rivers State'),
+(34, 'Sokoto State'),
+(35, 'Taraba State'),
+(36, 'Yobe State'),
+(37, 'Zamfara State');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `state`
+--
+ALTER TABLE `state`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `state`
+--
+ALTER TABLE `state`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ town.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 08, 2021 at 09:35 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dopatech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `town`
+--
+
+CREATE TABLE `town` (
+  `id` int(30) NOT NULL,
+  `state_id` int(30) NOT NULL,
+  `lga_id` int(30) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `town`
+--
+
+INSERT INTO `town` (`id`, `state_id`, `lga_id`, `name`, `date`) VALUES
+(1, 7, 129, 'eke', '2020-12-31 10:56:20');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `town`
+--
+ALTER TABLE `town`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `town`
+--
+ALTER TABLE `town`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
